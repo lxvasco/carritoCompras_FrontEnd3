8d796d11ad01593caee59b16b8b1aac5faa43e5c
lxvasco <laxivasco@gmail.com>
2022-08-31 21:32:49 -0500

    Carrito de compras

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
 function App() {
+
+  const [cantidad, setCantidad] = useState(0)
+
+  const aumentar = () => {
+    setCantidad(cantidad + 1)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecera cantidad={cantidad} />
+      <Listado func={aumentar} />
     </div>
   );
 }
